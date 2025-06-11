9cbfe1958b94bb378fefb0c98663e4b47b3bbf62
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-06-11 15:26:57 -0300

    Adiciona validação assíncrona de usuários no LoginHandler
    Inclui a classe `AuthRepository` para permitir a validação de usuários. O método `Handle` foi alterado para ser assíncrono, utilizando `await` para chamar `ValidateUserAsync`. A lógica de tratamento de resultados foi implementada, retornando `true` em caso de sucesso e lançando uma exceção de acesso não autorizado em caso de falha. O retorno padrão do método foi ajustado para `false`.

--- src/Auth.Application/Handlers/LoginHandler.cs
+++ src/Auth.Application/Handlers/LoginHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Auth.Application.Commands;
+using Auth.Infrastructure.Repositories;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,25 @@ namespace Auth.Application.Handlers
 {
     public class LoginHandler : IRequestHandler<LoginCommand, bool>
     {
+        public AuthRepository _repo { get; set; }
         public LoginHandler() { }
 
-        public Task<bool> Handle(LoginCommand request, CancellationToken cancellationToken)
+        public async Task<bool> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            await _repo.ValidateUserAsync(request.Username!, request.Password!)
+                .ContinueWith(task =>
+                {
+                    if (task.Result)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        throw new UnauthorizedAccessException("Invalid credentials");
+                    } 
+                });
+
+            return false;
         }
     }
 }
