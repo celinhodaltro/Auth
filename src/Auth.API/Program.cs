cf99b8cc03c721350aef6b76635a99e51a71f7bf
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-06-12 09:50:53 -0300

    Adicionar configuração de autenticação JwtBearer
    Implementada a configuração de autenticação usando o esquema JwtBearer, incluindo a definição de `ValidAudience` e `IssuerSigningKey` para validação de tokens JWT. Removido o registro do serviço `IAuthService`, indicando uma possível mudança na gestão da autenticação na aplicação.

--- src/Auth.API/Program.cs
+++ src/Auth.API/Program.cs
@@ -35,7 +35,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
